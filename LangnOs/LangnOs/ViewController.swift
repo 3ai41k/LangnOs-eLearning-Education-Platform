f228ff24b5804e73d36d78c0510e0561449bffe4
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-10-01 22:41:18 +0300

    Init project

---
+++ LangnOs/LangnOs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LangnOs
+//
+//  Created by Nikita Lizogubov on 01.10.2020.
+//  Copyright © 2020 NL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
