8c26157b7fd2b00c5d0160b3e681c4f234c1868f
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-10-12 22:18:50 +0300

    CoreData entities have been added

---
+++ LangnOs/LangnOs/BusinessLayer/Models/CoreDataEntities/VocabularyEntity.swift
@@ -0,0 +1,14 @@
+//
+//  VocabularyEntity.swift
+//  LangnOs
+//
+//  Created by Nikita Lizogubov on 12.10.2020.
+//  Copyright © 2020 NL. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+final class VocabularyEntity: NSManagedObject {
+    
+}
