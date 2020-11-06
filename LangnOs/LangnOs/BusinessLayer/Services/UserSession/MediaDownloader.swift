fb399a03069108a02e876538f96117317a54a340
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-11-06 17:53:24 +0200

    Add urlCache to the MediaDownloader

--- LangnOs/LangnOs/BusinessLayer/Services/UserSession/MediaDownloader.swift
+++ LangnOs/LangnOs/BusinessLayer/Services/UserSession/MediaDownloader.swift
@@ -12,22 +12,46 @@ protocol MediaDownloadableProtocol {
     func downloadMedia(url: URL, onSucces: @escaping (Data) -> Void, onFailure: @escaping (Error) -> Void)
 }
 
-final class MediaDownloader { }
+final class MediaDownloader {
+    
+    // MARK: - Private properties
+    
+    private var session: URLSession
+    
+    init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.urlCache = URLCache(memoryCapacity: 50 * DataSize.MB,
+                                          diskCapacity: 50 * DataSize.MB,
+                                          diskPath: "images")
+        configuration.httpMaximumConnectionsPerHost = 5
+        
+        session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+    }
+    
+}
 
 // MARK: - MediaDownloadableProtocol
 
 extension MediaDownloader: MediaDownloadableProtocol {
     
     func downloadMedia(url: URL, onSucces: @escaping (Data) -> Void, onFailure: @escaping (Error) -> Void) {
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            DispatchQueue.main.async {
-                if let error = error {
-                    onFailure(error)
-                } else if let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data {
-                    onSucces(data)
-                }
+        session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                onFailure(error)
+            } else if let response = response as? HTTPURLResponse, response.statusCode == 200, let data = data {
+                onSucces(data)
             }
         }.resume()
+    }
+    
+}
+
+// MARK: - DataSize
+
+extension MediaDownloader {
+    
+    private enum DataSize {
+        static let MB = 1024 * 1024
     }
     
 }
