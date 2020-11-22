dab32f053e94e97085c7daae9629a87d0529de66
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2020-11-22 17:27:48 +0200

    Clear User Entity

--- LangnOs/LangnOs/BusinessLayer/Models/User.swift
+++ LangnOs/LangnOs/BusinessLayer/Models/User.swift
@@ -13,16 +13,12 @@ class User1: Codable {
     
     // MARK: - Public properties
     
-    enum CodingKeys: String, CodingKey {
-        case id, name, email, phone, password, photoURL, rights
-    }
-    
     let id: String
     var name: String
     var email: String
     var phone: String
     var password: String
-    @Published var photoURL: URL?
+    var photoURL: URL?
     var rights: [Right]
     
     static var empty: User1 {
@@ -41,30 +37,6 @@ class User1: Codable {
         self.password = password
         self.photoURL = photoURL
         self.rights = rights
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(String.self, forKey: .id)
-        name = try container.decode(String.self, forKey: .name)
-        email = try container.decode(String.self, forKey: .email)
-        phone = try container.decode(String.self, forKey: .phone)
-        password = try container.decode(String.self, forKey: .password)
-        photoURL = try container.decode(URL.self, forKey: .photoURL)
-        rights = try container.decode([Right].self, forKey: .rights)
-    }
-    
-    // MARK: - Public methods
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(name, forKey: .name)
-        try container.encode(email, forKey: .email)
-        try container.encode(phone, forKey: .phone)
-        try container.encode(password, forKey: .password)
-        try container.encode(photoURL, forKey: .photoURL)
-        try container.encode(rights, forKey: .rights)
     }
     
 }
